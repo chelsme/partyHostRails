abfb919a1fdc55dc74729971228ee3b8ef9c801f
Chelsea Crowson <chelsea.crowson@me.com>
2018-11-28 10:44:17 -0600

    update task serializer

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -1,5 +1,5 @@
 class TaskSerializer < ActiveModel::Serializer
-  attributes :id, :action, :party_id, :guest_id
+  attributes :id, :action, :party_id, :guest_id, :guest
   belongs_to :guest
   belongs_to :party
 end
